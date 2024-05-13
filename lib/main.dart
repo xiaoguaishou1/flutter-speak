6fbe949ec29299dfc09558783ec32efa31789cc2
panghu <tompanghu@gmail.com>
2024-05-13 15:57:59 +0800

    增加启动屏

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,28 @@
  * @Author: panghu tompanghu@gmail.com
  * @Date: 2024-04-29 11:04:59
  * @LastEditors: panghu tompanghu@gmail.com
- * @LastEditTime: 2024-05-06 16:17:35
+ * @LastEditTime: 2024-05-13 14:52:07
  * @FilePath: /speak/lib/main.dart
  * @Description: main dart
  */
 import 'package:flutter/material.dart';
 import 'package:speak/router/index.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  print('ready in 3...');
+  await Future.delayed(const Duration(seconds: 1));
+  print('ready in 2...');
+  await Future.delayed(const Duration(seconds: 1));
+  print('ready in 1...');
+  await Future.delayed(const Duration(seconds: 1));
+  print('go!');
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
--- lib/page/chat/index.dart
+++ lib/page/chat/index.dart
@@ -2,7 +2,7 @@
  * @Author: panghu tompanghu@gmail.com
  * @Date: 2024-05-07 17:07:08
  * @LastEditors: panghu tompanghu@gmail.com
- * @LastEditTime: 2024-05-10 11:05:57
+ * @LastEditTime: 2024-05-11 09:21:55
  * @FilePath: /speak/lib/page/chat/index.dart
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -221,7 +221,6 @@ class SendMessage extends HookWidget {
                       onPressed: () {
                         // 处理发送按钮点击事件
                         onSend(_controller.value.text);
-
                         StreamSSE(
                           'https://open.bigmodel.cn/api/paas/v4/chat/completions',
                           data: {
@@ -237,7 +236,6 @@ class SendMessage extends HookWidget {
                           },
                           onData: handleData,
                         );
-
                         _controller.clear();
                       },
                     ),
